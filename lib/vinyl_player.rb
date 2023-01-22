f08636cf705c3a0fe8dd93b2f4e869348ee01a39
callumahern <callum.ahern@simplybusiness.co.uk>
2023-01-22 10:37:30 +0000

    SRP for error messages

--- lib/vinyl_player.rb
+++ lib/vinyl_player.rb
@@ -9,14 +9,24 @@ class VinylPlayer
   end
 
   def turn_on
-    raise 'Turntable is already on' if @state == :on
+    turnable_already_on_error?
 
     @state = :on
   end
 
   def turn_off
-    raise 'Turntable is already off' if @state == :off
+    turnable_already_off_error?
 
     @state = :off
+  end
+
+  private
+
+  def turnable_already_on_error?
+    raise 'Turntable is already on' if @state == :on
+  end
+
+  def turnable_already_off_error?
+    raise 'Turntable is already off' if @state == :off
   end
 end
